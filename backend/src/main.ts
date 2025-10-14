ca280989e3a97a548759289cb359b1d0676c3bd9
Elena Romanova <romanova.bim@gmail.com>
2025-10-14 19:42:09 +0500

    корректировка 1 по результатам автотестов

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
+// Поддержка crypto.randomUUID в CommonJS
+if (!globalThis.crypto) {
+  globalThis.crypto = {} as any;
+}
+
+if (!globalThis.crypto.randomUUID) {
+  const nodeCrypto = require('crypto');
+  globalThis.crypto.randomUUID = () => nodeCrypto.randomUUID();
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
   await app.listen(3000);
 }
+
 bootstrap();
